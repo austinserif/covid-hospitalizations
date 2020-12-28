f4fb4190bb6ad6a9951a9646801252d1a55f08c9
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-12-27 19:19:07 -0800

    removed styletron and base ui

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import './App.css';
 import Routes from './components/Routes';
 
-//baseui imports
-import {Client as Styletron} from 'styletron-engine-atomic';
-import {Provider as StyletronProvider} from 'styletron-react';
-import {LightTheme, BaseProvider, styled} from 'baseui';
-
-const engine = new Styletron();
-
-const Centered = styled('div', {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '100%',
-});
-
 function App() {
   return (
-    <StyletronProvider value={engine}>
-      <BaseProvider theme={LightTheme}>
-        <Centered>
-          <div className="App">
-            <Routes/>
-          </div>  
-        </Centered>
-      </BaseProvider>
-    </StyletronProvider>
-
+    <div className="App">
+      <Routes/>
+    </div>
   );
 }
 
